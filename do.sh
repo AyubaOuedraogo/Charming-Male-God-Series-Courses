81e8f9c263312d522041906ecc6ec1aa3dd1aa51
Bob <SBob.com>
2024-06-23 03:13:06 +0800

    first

---
+++ do.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+git add 09.魅力男神系列之完美自信.zip.03*
+git commit -m "first"
+git push origin main
+git add 09.魅力男神系列之完美自信.zip.04*
+git commit -m "first"
+git push origin main
+git add 09.魅力男神系列之完美自信.zip.06*
+git commit -m "first"
+git push origin main
+git add 09.魅力男神系列之完美自信.zip.05*
+git commit -m "first"
+git push origin main
+git add 09.魅力男神系列之完美自信.zip.07*
+git commit -m "first"
+git push origin main
+git add 10.魅力男神系列之新手聊天模板.zip.00*
+git commit -m "first"
+git push origin main
+git add 10.魅力男神系列之新手聊天模板.zip.01*
+git commit -m "first"
+git push origin main
+git add 10.魅力男神系列之新手聊天模板.zip.02*
+git commit -m "first"
+git push origin main
+git add 10.魅力男神系列之新手聊天模板.zip.03*
+git commit -m "first"
+git push origin main
+git add 10.魅力男神系列之新手聊天模板.zip.04*
+git commit -m "first"
+git push origin main
+git add 10.魅力男神系列之新手聊天模板.zip.05*
+git commit -m "first"
+git push origin main
+git add 10.魅力男神系列之新手聊天模板.zip.06*
+git commit -m "first"
+git push origin main
+git add 10.魅力男神系列之新手聊天模板.zip.07*
+git commit -m "first"
+git push origin main
+git add 10.魅力男神系列之新手聊天模板.zip.08*
+git commit -m "first"
+git push origin main
+git add 10.魅力男神系列之新手聊天模板.zip.09*
+git commit -m "first"
+git push origin main
+git add .
+git commit -m "first"
+git push origin main
